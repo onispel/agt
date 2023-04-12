63d2f86c716b74f013728d34110ff26872c7ecca
Oliver Nispel <oliver@nispel.org>
2023-04-12 15:34:52 +0200

    feature integratoni

--- agt/main.py
+++ agt/main.py
@@ -1,14 +1,28 @@
 import click
-from agt import __version__
 
-import agt.version
+import agt
+from agt import (
+    version,
+    vaults,
+    jobs,
+)
+
+
+#read AccessKey and SecretKey from environment variables
+# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
 
 @click.group
 def main_cli() -> None:
     """AWS Glacier Tools CLI"""
+
     pass
 
-main_cli.add_command(agt.version.version)
+main_cli.add_command(version.version)
+main_cli.add_command(vaults.vaults_grp)
+main_cli.add_command(jobs.jobs_grp)
 
 if __name__ == '__main__':
+
     main_cli()
