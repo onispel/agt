b0abf2f3fdd380e4afbf27977816e28a980cc937
Oliver Nispel <oliver@nispel.org>
2023-04-13 18:48:40 +0200

    describe vault added, formatting

--- agt/vaults.py
+++ agt/vaults.py
@@ -1,12 +1,17 @@
 import click
 
 from agt import client
-from agt.format import format_vaults_table, format_vaults_json
+from agt.format import format_table, format_json, format_list
 
 
 def list_vaults() -> dict:
     """List all vaults"""
     response = client.list_vaults()['VaultList']
+    return response
+
+def describe_vault(vault_name: str) -> dict:
+    """Describe a vault"""
+    response = client.describe_vault(vaultName=vault_name)
     return response
 
 
@@ -17,6 +22,7 @@ def vaults_grp() -> None:
 
 @vaults_grp.command(name='list')
 @click.option('--table', 'outfmt', flag_value='table', default=True, help='Show table (default)')
+@click.option('--list', 'outfmt', flag_value='list', default=True, help='Show list')
 @click.option('--json', 'outfmt', flag_value='json', help='Show json')
 def list_vaults_cli(outfmt) -> None:
     """List all vaults"""
@@ -24,8 +30,26 @@ def list_vaults_cli(outfmt) -> None:
     response = list_vaults()
 
     match outfmt:
-        case 'table': output = format_vaults_table(response,exclude=['VaultARN'],fmt='psql')
-        case 'json': output = format_vaults_json(response)
+        case 'table': output = format_table(response,exclude=['VaultARN'],fmt='psql')
+        case 'list': output = format_list(response,exclude=['VaultARN'],fmt='psql')
+        case 'json': output = format_json(response)
 
     click.echo(output)
 
+
+@vaults_grp.command(name='describe')
+@click.argument('vault_name')
+@click.option('--table', 'outfmt', flag_value='table', help='Show table')
+@click.option('--list', 'outfmt', flag_value='list', default=True, help='Show json (default)')
+@click.option('--json', 'outfmt', flag_value='json', help='Show json (default)')
+def describe_vault_cli(vault_name: str, outfmt:str) -> None:
+    """Describe a vault"""
+
+    response = describe_vault(vault_name)
+
+    match outfmt:
+        case 'table': output = format_table(response,exclude=['VaultARN','ResponseMetadata'],fmt='psql')
+        case 'list': output = format_list(response,exclude=['VaultARN','ResponseMetadata'],fmt='psql')
+        case 'json': output = format_json(response)
+
+    click.echo(output)
